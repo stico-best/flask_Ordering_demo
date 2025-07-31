70ed4b06fe42f92be5c659dccdb9ccd33305a5dc
stico-best <1042609446@qq.com>
2025-07-31 19:38:08 +0800

    2025.07.30-餐厅主页更新推送

--- app/main.py
+++ app/main.py
@@ -5,14 +5,14 @@ import json
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)  # 随机密钥
+
+# 加载日志模块
 logger = Logger(log_file_path='app.log', log_level='INFO')
 
-# 前期用dict代替, 后期做link mysql用户登录表
-user_login = {
-    'root': '123456'
-}
+# 登录页用户名密码, 前期用Json key-value代替, 后期做link mysql用户登录表
+user_json_path = 'database/'
+user_json_file = 'user.json'
 
-user_json_path = 'database/user.json'
 
 @app.route('/')
 def login_page():
@@ -27,27 +27,61 @@ def login():
         password = request.form.get('password')
 
         try:
-            with open(user_json_path, 'r') as f:
-                users = json.load(f)  # users: list[{u,p}, {u,p}]
-            logger.info(f'用户账号密码Json已读取')
+            with open(os.path.join(user_json_path, user_json_file), 'r') as f:
+                users_list = json.load(f)  # users: list[{u,p}, {u,p}]
+            logger.info(f'/login load Json file success.')
         except FileNotFoundError:
-            return jsonify({'success': False, 'message': '用户数据文件不存在'}), 500
+            return jsonify({'success': False, 'message': 'Json file not found.'}), 500
 
         user_found = False
-        for user in users:
-            if user["username"] == username and user["password"] == password:
+        for users_index in users_list:
+            if users_index["username"] == username and users_index["password"] == password:
                 session['username'] = username
                 logger.info(f"user '{username}' login to system.")
-                return jsonify({'success': True, 'message': '登录成功'})
+                return jsonify({'success': True, 'message': 'login success'})
 
-        logger.error('用户名或密码错误')
-        return jsonify({'success': False, 'message': '用户名或密码错误'})
+        logger.error('username or password error.')
+        return jsonify({'success': False, 'message': 'username or password error.'})
     # 当GET请求时
     return render_template('login.html', message='')
 
-# @app.route('/register', methods=['POST'])
-# def register():
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
 
+        try:
+            with open(os.path.join(user_json_path, user_json_file), 'r') as f:
+                users_list = json.load(f)
+            logger.info('/register load json file success.')
+        except FileNotFoundError:
+            if not os.path.exists(user_json_path):
+                os.makedirs(user_json_path)
+                with open(os.path.join(user_json_path, user_json_file), 'w') as f:
+                    f.write('')
+            users_list = []
+            # return jsonify({'success': False, 'message': 'Json file not found.'}), 500
+        # print(f'users_list: {users_list}, {type(users_list)}')
+
+        user_found = False
+        for users_index in users_list:
+            if users_index["username"] == username:
+                logger.info(f'user {username} has exists, please input correct password.')
+                return render_template('login.html', message='')
+
+        user = {}  # 待注册新用户dict
+        if not user_found:
+            user['username'] = username
+            user['password'] = password
+        users_list.append(user)
+
+        try:
+            with open(os.path.join(user_json_path, user_json_file), 'w') as f:
+                json.dump(users_list, f, indent=4)
+            return jsonify({'success': True, 'message': 'register success.'})
+        except Exception as e:
+            return jsonify({'success': False, 'message': f'register fail: {str(e)}'}), 500
 
 @app.route('/home')
 def home():
